be315b041e622feee766d8dac6771b6573107398
Florent <118511007+fllin1@users.noreply.github.com>
2025-10-24 12:03:06 +0000

    fix: Refacto of cache into Client

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -2,51 +2,49 @@ package pokeapi
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
-
-	"github.com/fllin1/pokedexcli/internal/pokecache"
 )
 
 // ListLocations -
-func (c *Client) ListLocations(pageURL *string, cache *pokecache.Cache) (RespShallowLocations, error) {
+func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 	url := baseURL + "/location-area?offset=0&limit=20"
 	if pageURL != nil {
 		url = *pageURL
 	}
 
-	var dat []byte
-
-	if data, exists := cache.Get(url); exists {
-		fmt.Println("Cache hit for URL:", url)
-		dat = data
-	} else {
-		fmt.Println("Cache miss for URL:", url)
-		req, err := http.NewRequest("GET", url, nil)
+	if val, ok := c.cache.Get(url); ok {
+		locationsResp := RespShallowLocations{}
+		err := json.Unmarshal(val, &locationsResp)
 		if err != nil {
 			return RespShallowLocations{}, err
 		}
 
-		resp, err := c.httpClient.Do(req)
-		if err != nil {
-			return RespShallowLocations{}, err
-		}
-		defer resp.Body.Close()
-
-		dat, err = io.ReadAll(resp.Body)
-		if err != nil {
-			return RespShallowLocations{}, err
-		}
-
-		cache.Add(url, dat)
+		return locationsResp, nil
 	}
 
-	locationsResp := RespShallowLocations{}
-	err := json.Unmarshal(dat, &locationsResp)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return RespShallowLocations{}, err
 	}
 
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return RespShallowLocations{}, err
+	}
+	defer resp.Body.Close()
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return RespShallowLocations{}, err
+	}
+
+	locationsResp := RespShallowLocations{}
+	err = json.Unmarshal(dat, &locationsResp)
+	if err != nil {
+		return RespShallowLocations{}, err
+	}
+
+	c.cache.Add(url, dat)
 	return locationsResp, nil
 }
--- main.go
+++ main.go
@@ -4,15 +4,12 @@ import (
 	"time"
 
 	"github.com/fllin1/pokedexcli/internal/pokeapi"
-	"github.com/fllin1/pokedexcli/internal/pokecache"
 )
 
 func main() {
-	pokeClient := pokeapi.NewClient(5 * time.Second)
-	cache := pokecache.NewCache(5 * time.Second)
+	pokeClient := pokeapi.NewClient(5 * time.Second, 5 * time.Minute)
 	cfg := &config{
 		pokeapiClient: pokeClient,
-		cache:		   cache,
 	}
 
 	startRepl(cfg)
